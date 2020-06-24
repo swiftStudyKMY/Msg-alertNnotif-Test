6905bca3217ecdd985bf9670f9c5159bc9256021
KMY <muziq43@hanafn.com>
2020-06-24 15:56:38 +0900

    UIAlertController, UIAlertAction 예제

--- Msg-alertNnotif-Test/ViewController.swift
+++ Msg-alertNnotif-Test/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var res: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func alertBtn(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "제목", message: "항목을 선택해주세요.", preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        
+        let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
---
+++ Msg-alertNnotif-Test/ViewController2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  Msg-alertNnotif-Test
+//
+//  Created by 김민영 on 2020/06/24.
+//  Copyright © 2020 KIMMINYOUNG. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController2 : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
