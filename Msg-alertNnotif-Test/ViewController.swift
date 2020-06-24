9552aea07be067803c1d601d503e2c63077ce391
KMY <muziq43@hanafn.com>
2020-06-24 17:16:39 +0900

    alert에 textFiled 추가 및 입력

--- Msg-alertNnotif-Test/ViewController.swift
+++ Msg-alertNnotif-Test/ViewController.swift
@@ -12,21 +12,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var res: UILabel!
     
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
     @IBAction func alertBtn(_ sender: Any) {
         
         let alert = UIAlertController(title: "제목", message: "항목을 선택해주세요.", preferredStyle: .alert) //두개 이상부터는 하단으로 내려간다.
         
-        let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
-        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: {(_) in
+            self.res.text = "확인 버튼 클릭"
+        })
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: {(_) in
+            self.res.text = "취소 버튼 클릭"
+        })
 
-        let exec = UIAlertAction(title: "실행", style: .destructive, handler: nil)    //빨간색
-        let stop = UIAlertAction(title: "중지", style: .default, handler: nil)
+        let exec = UIAlertAction(title: "실행", style: .destructive, handler: {(_) in
+            self.res.text = "실행 버튼 클릭"
+        })
+        let stop = UIAlertAction(title: "중지", style: .default, handler: {(_) in
+            self.res.text = "중지 버튼 클릭"
+        })
         
         alert.addAction(ok)
         alert.addAction(cancel)
@@ -37,6 +50,45 @@ class ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
         
     }
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        let title = "iTunes store에 로그인"
+        
+        let msg = "사용자의 Apple ID  801sanae@gmail.com의 암호를 입력하시오."
+        
+        let alert1 = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        
+        let cancel1 = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        let ok1 = UIAlertAction(title: "확인", style: .default, handler: {(_) in
+            if let tf = alert1.textFields?[0] {
+                print(tf.text!)
+//                print(">>>\(alert1.textFields?.first!.text)")
+            }else{
+                print("입력된 값은 없다.")
+            }
+        })
+        
+        alert1.addAction(cancel1)
+        alert1.addAction(ok1)
+
+//        alert1.addTextField(configurationHandler: <#T##((UITextField) -> Void)?##((UITextField) -> Void)?##(UITextField) -> Void#>)
+        alert1.addTextField { (tf) in
+            tf.placeholder = "암호"
+            tf.isSecureTextEntry = true
+        }
+
+        
+        
+        
+        present(alert1, animated: false)
+        
+
+        
+        
+        
+    }
+    
     
 }
 
