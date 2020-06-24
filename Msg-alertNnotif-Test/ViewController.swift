46924cdbfcc6a61819ca0c97751c2d7c194a15ab
KMY <muziq43@hanafn.com>
2020-06-24 15:27:08 +0900

    Initial Commit

---
+++ Msg-alertNnotif-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Msg-alertNnotif-Test
+//
+//  Created by 김민영 on 2020/06/24.
+//  Copyright © 2020 KIMMINYOUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
