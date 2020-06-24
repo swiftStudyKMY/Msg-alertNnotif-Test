85a04799d86266709c17911ac552b5b4f442c4cd
KMY <muziq43@hanafn.com>
2020-06-24 15:58:57 +0900

    .alert 스타일의 주석 추가.

--- Msg-alertNnotif-Test/ViewController.swift
+++ Msg-alertNnotif-Test/ViewController.swift
@@ -20,15 +20,19 @@ class ViewController: UIViewController {
 
     @IBAction func alertBtn(_ sender: Any) {
         
-        let alert = UIAlertController(title: "제목", message: "항목을 선택해주세요.", preferredStyle: .alert)
-        
-        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-        
+        let alert = UIAlertController(title: "제목", message: "항목을 선택해주세요.", preferredStyle: .alert) //두개 이상부터는 하단으로 내려간다.
         
         let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        let exec = UIAlertAction(title: "실행", style: .destructive, handler: nil)    //빨간색
+        let stop = UIAlertAction(title: "중지", style: .default, handler: nil)
         
         alert.addAction(ok)
         alert.addAction(cancel)
+        
+        alert.addAction(exec)
+        alert.addAction(stop)
         
         self.present(alert, animated: true, completion: nil)
         
